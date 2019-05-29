689ca0462db47293ac04bbaba54029a233af8364
brentru <robots199@me.com>
2019-05-29 17:05:43 -0400

    adding init/deinit/brightness methods

--- adafruit_rgbled.py
+++ adafruit_rgbled.py
@@ -36,8 +36,34 @@ Implementation Notes
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 """
-
-# imports
+import math
+from board import *
+import pulseio
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_RGBLED.git"
+
+class RGBLED:
+    """
+    RGB LED Driver Class.
+    """
+    def init(self, red_pin, green_pin, blue_pin, brightness=1.0):
+        """Initializes a RGB LED.
+        :param int red_pin: Red Anode Pin.
+        :param int green_pin: Green Anode Pin.
+        :param int blue_pin: Blue Anode Pin.
+        """
+        self._red_led = pulseio.PWMOut(red_pin, variable_frequency=True)
+        self._green_led = pulseio.PWMOut(green_pin, variable_frequency=True)
+        self._blue_led = pulseio.PWMOut(blue_pin, variable_frequency=True)
+        self.brightness = brightness
+    
+    def deinit(self):
+        """Turn the LEDs off, deinit pwmout and release hardware resources.
+        """
+        self._red_led.duty_cycle = 0
+        self._green_led.duty_cycle = 0
+        self._blue_led.duty_cycle = 0
+        self._red_led.deinit()
+        self._blue_led.deinit()
+        self._green_led.deinit()
